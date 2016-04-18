f4e638134197cce657cc29aa725fb0c263e4e08f
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-04-18 19:11:06 +0100

    Initial Spring commit

---
+++ src/main/java/com/uk/jacob/SimplePingApplication.java
@@ -0,0 +1,12 @@
+package com.uk.jacob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplePingApplication.class, args);
+	}
+}
